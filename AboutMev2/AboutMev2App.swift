b0102feae46848c8ac3ed4e1c61c9a463a3c01a1
Dylan Borgio <dylanborgio24@gmail.com>
2024-08-23 08:22:29 -0500

    Initial Commit

---
+++ AboutMev2/AboutMev2App.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMev2App.swift
+//  AboutMev2
+//
+//  Created by Dylan Borgio on 8/23/24.
+//
+
+import SwiftUI
+
+@main
+struct AboutMev2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
